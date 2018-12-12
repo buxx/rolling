cc60d8e48638ac6af6d601053c6239c7eb397f1d
Bastien Sevajol <sevajol.bastien@gmail.com>
2018-12-12 21:58:06 +0100

    add offsets to map display

--- rolling/gui/map/render.py
+++ rolling/gui/map/render.py
@@ -24,7 +24,13 @@ class WorldMapRenderEngine(object):
     def attributes(self):
         return self._attributes
 
-    def render(self, width: int, height: int) -> None:
+    def render(
+        self,
+        width: int,
+        height: int,
+        offset_horizontal: int = 0,
+        offset_vertical: int = 0,
+    ) -> None:
         map_width = self._world_map_source.geography.width
         map_height = self._world_map_source.geography.height
 
@@ -39,28 +45,34 @@ class WorldMapRenderEngine(object):
         if width_difference > 1:
             left_void = width_difference // 2
             right_void = left_void + 1 if width_difference % 2 else left_void
+            left_void = left_void + offset_horizontal
+            right_void = right_void - offset_horizontal
         elif width_difference == 1:
-            left_void = 1
-            right_void = 0
+            left_void = 1 + offset_horizontal
+            right_void = 0 - offset_horizontal
         else:
-            left_void = 0
-            right_void = 0
+            left_void = 0 + offset_horizontal
+            right_void = 0 - offset_horizontal
 
         # compute static void top and bottom
         height_difference = height - map_height
         if height_difference > 1:
             top_void = height_difference // 2
             bottom_void = top_void + 1 if height_difference % 2 else top_void
+            top_void = top_void + offset_vertical
+            bottom_void = bottom_void - offset_vertical
         elif height_difference == 1:
-            top_void = 1
-            bottom_void = 0
+            top_void = 1 + offset_vertical
+            bottom_void = 0 - offset_vertical
         else:
-            top_void = 0
-            bottom_void = 0
+            top_void = 0 + offset_vertical
+            bottom_void = 0 - offset_vertical
 
         # prepare void values
         self._rows = [default_str * width] * top_void
-        map_display_height = map_height if height_difference > 0 else height
+        map_display_height = (map_height if height_difference > 0 else height) - abs(
+            offset_vertical
+        )
         self._rows.extend([default_str * left_void] * map_display_height)
         self._rows.extend([default_str * width] * bottom_void)
         self._attributes: typing.List[
@@ -69,9 +81,18 @@ class WorldMapRenderEngine(object):
 
         # fill rows and attributes line per line
         for row_i, row in enumerate(self._world_map_source.geography.rows):
+            row_left_void = left_void
+            row_right_void = right_void
+
+            if (row_i + top_void) < 0:
+                continue
 
             # do not fill outside screen
             if row_i == height:
+                break
+
+            # do not fill outside screen
+            if row_i + top_void + 1 > height:
                 break
 
             for col_i, col in enumerate(row):
@@ -79,6 +100,16 @@ class WorldMapRenderEngine(object):
                 # do not fill outside screen
                 if col_i == width:
                     break
+
+                # Avoid char if left void need to suppress it
+                if row_left_void < 0:
+                    row_left_void += 1
+                    continue
+
+                # Avoid chars if right void need to suppress them
+                if row_right_void < 0:
+                    if col_i == len(row) + row_right_void:
+                        break
 
                 tile_str = self._world_map_source.legend.get_str_with_type(col)
                 self._rows[row_i + top_void] += tile_str
--- tests/unit/gui/test_world_map_render.py
+++ tests/unit/gui/test_world_map_render.py
@@ -21,6 +21,126 @@ class TestWorldMapRender(object):
             [(world.Sea.get_full_id(), 4)],
         ] == engine.attributes
 
+    def test_unit__render__ok__just_space_with_horizontal_less_1_offset(self, worldmapsourceb_txt: str):
+        source = WorldMapSource(worldmapsourceb_txt)
+        engine = WorldMapRenderEngine(source)
+        engine.render(width=4, height=3, offset_horizontal=-1)
+
+        assert [b"~~~ ", b"^^~ ", b"~~~ "] == engine.rows
+        assert [
+            [(world.Sea.get_full_id(), 3), (None, 1)],
+            [
+                (world.Mountain.get_full_id(), 2),
+                (world.Sea.get_full_id(), 1),
+                (None, 1),
+            ],
+            [(world.Sea.get_full_id(), 3), (None, 1)],
+        ] == engine.attributes
+
+    def test_unit__render__ok__just_space_with_horizontal_more_1_offset(self, worldmapsourceb_txt: str):
+        source = WorldMapSource(worldmapsourceb_txt)
+        engine = WorldMapRenderEngine(source)
+        engine.render(width=4, height=3, offset_horizontal=1)
+
+        assert [b" ~~~", b" ~^^", b" ~~~"] == engine.rows
+        assert [
+            [(None, 1), (world.Sea.get_full_id(), 3)],
+            [
+                (None, 1),
+                (world.Sea.get_full_id(), 1),
+                (world.Mountain.get_full_id(), 2),
+            ],
+            [(None, 1), (world.Sea.get_full_id(), 3)],
+        ] == engine.attributes
+
+    def test_unit__render__ok__just_space_with_horizontal_more_2_offset(self, worldmapsourceb_txt: str):
+        source = WorldMapSource(worldmapsourceb_txt)
+        engine = WorldMapRenderEngine(source)
+        engine.render(width=4, height=3, offset_horizontal=2)
+
+        assert [b"  ~~", b"  ~^", b"  ~~"] == engine.rows
+        assert [
+            [(None, 2), (world.Sea.get_full_id(), 2)],
+            [
+                (None, 2),
+                (world.Sea.get_full_id(), 1),
+                (world.Mountain.get_full_id(), 1),
+            ],
+            [(None, 2), (world.Sea.get_full_id(), 2)],
+        ] == engine.attributes
+
+    def test_unit__render__ok__just_space_with_horizontal_less_2_offset(self, worldmapsourceb_txt: str):
+        source = WorldMapSource(worldmapsourceb_txt)
+        engine = WorldMapRenderEngine(source)
+        engine.render(width=4, height=3, offset_horizontal=-2)
+
+        assert [b"~~  ", b"^~  ", b"~~  "] == engine.rows
+        assert [
+            [(world.Sea.get_full_id(), 2), (None, 2)],
+            [
+                (world.Mountain.get_full_id(), 1),
+                (world.Sea.get_full_id(), 1),
+                (None, 2),
+            ],
+            [(world.Sea.get_full_id(), 2), (None, 2)],
+        ] == engine.attributes
+
+    def test_unit__render__ok__just_space_with_vertical_less_1_offset(self, worldmapsourceb_txt: str):
+        source = WorldMapSource(worldmapsourceb_txt)
+        engine = WorldMapRenderEngine(source)
+        engine.render(width=4, height=3, offset_vertical=-1)
+
+        assert [b"~^^~", b"~~~~", b"    "] == engine.rows
+        assert [
+            [
+                (world.Sea.get_full_id(), 1),
+                (world.Mountain.get_full_id(), 2),
+                (world.Sea.get_full_id(), 1),
+            ],
+            [(world.Sea.get_full_id(), 4)],
+            [(None, 4)],
+        ] == engine.attributes
+
+    def test_unit__render__ok__just_space_with_vertical_more_1_offset(self, worldmapsourceb_txt: str):
+        source = WorldMapSource(worldmapsourceb_txt)
+        engine = WorldMapRenderEngine(source)
+        engine.render(width=4, height=3, offset_vertical=1)
+
+        assert [b"    ", b"~~~~", b"~^^~"] == engine.rows
+        assert [
+            [(None, 4)],
+            [(world.Sea.get_full_id(), 4)],
+            [
+                (world.Sea.get_full_id(), 1),
+                (world.Mountain.get_full_id(), 2),
+                (world.Sea.get_full_id(), 1),
+            ],
+        ] == engine.attributes
+
+    def test_unit__render__ok__just_space_with_vertical_less_2_offset(self, worldmapsourceb_txt: str):
+        source = WorldMapSource(worldmapsourceb_txt)
+        engine = WorldMapRenderEngine(source)
+        engine.render(width=4, height=3, offset_vertical=-2)
+
+        assert [b"~~~~", b"    ", b"    "] == engine.rows
+        assert [
+            [(world.Sea.get_full_id(), 4)],
+            [(None, 4)],
+            [(None, 4)],
+        ] == engine.attributes
+
+    def test_unit__render__ok__just_space_with_vertical_more_2_offset(self, worldmapsourceb_txt: str):
+        source = WorldMapSource(worldmapsourceb_txt)
+        engine = WorldMapRenderEngine(source)
+        engine.render(width=4, height=3, offset_vertical=2)
+
+        assert [b"    ", b"    ", b"~~~~"] == engine.rows
+        assert [
+            [(None, 4)],
+            [(None, 4)],
+            [(world.Sea.get_full_id(), 4)],
+        ] == engine.attributes
+
     def test_unit__render__ok__height_more_one(self, worldmapsourceb_txt: str):
         source = WorldMapSource(worldmapsourceb_txt)
         engine = WorldMapRenderEngine(source)
