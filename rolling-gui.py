d18c911c36f7adbeda1cdc6b62b5031c21a440b9
Bastien Sevajol <sevajol.bastien@gmail.com>
2019-11-05 19:02:49 +0100

    add startup script for gui

---
+++ rolling-gui.py
@@ -0,0 +1,11 @@
+# coding: utf-8
+from sqlalchemy.sql import default_comparator
+
+
+def main():
+    from rolling.gui.run import main as photographer_gui_run
+    photographer_gui_run()
+
+
+if __name__ == "__main__":
+    main()
