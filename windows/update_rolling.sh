017ea3a0e2b5a23b8dec2d5f113068859f433aa1
Bastien Sevajol <sevajol.bastien@gmail.com>
2019-11-07 08:55:30 +0100

    wip

--- windows/update_rolling.sh
+++ windows/update_rolling.sh
@@ -1,1 +1,11 @@
-#!/bin/bashecho Move to rolling directorycd ~/rollingecho Activate python virtual envsource venv/bin/activateecho Update python dependenciespip install -e ".[tui]"echo Update rollingpython setup.py developecho Update finished
+#!/bin/bash
+echo Move to rolling directory
+cd ~/rolling
+git pull origin
+echo Activate python virtual env
+source venv/bin/activate
+echo Update python dependencies
+pip install -e ".[tui]"
+echo Update rolling
+python setup.py develop
+echo Update finished
