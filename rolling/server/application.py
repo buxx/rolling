322aef6e1280abf41a3d5a3424506fb0090f382f
Bastien Sevajol <sevajol.bastien@gmail.com>
2020-12-24 13:36:57 +0100

    add api version

--- rolling/server/application.py
+++ rolling/server/application.py
@@ -10,6 +10,7 @@ from rolling.server.controller.business import BusinessController
 from rolling.server.controller.character import CharacterController
 from rolling.server.controller.common import CommonController
 from rolling.server.controller.conversation import ConversationController
+from rolling.server.controller.system import SystemController
 from rolling.server.controller.world import WorldController
 from rolling.server.controller.zone import ZoneController
 
@@ -28,5 +29,6 @@ def get_application(kernel: Kernel) -> Application:
     BusinessController(kernel).bind(app)
     BusinessController(kernel).bind(app)
     AdminController(kernel).bind(app)
+    SystemController(kernel).bind(app)
 
     return app
---
+++ rolling/server/controller/system.py
@@ -0,0 +1,24 @@
+# coding: utf-8
+from aiohttp import web
+from aiohttp.web_app import Application
+from aiohttp.web_request import Request
+from aiohttp.web_response import Response
+import pkg_resources
+
+from rolling.kernel import Kernel
+from rolling.server.controller.base import BaseController
+from rolling.server.extension import hapic
+
+version = pkg_resources.require("rolling")[0].version
+
+
+class SystemController(BaseController):
+    def __init__(self, kernel: Kernel) -> None:
+        super().__init__(kernel)
+
+    @hapic.with_api_doc()
+    async def version(self, request: Request) -> Response:
+        return Response(status=200, body=version)
+
+    def bind(self, app: Application) -> None:
+        app.add_routes([web.get("/system/version", self.version)])
