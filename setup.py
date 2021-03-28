70b0a6f55953896f2c4cff6b467de100774fa918
Bastien Sevajol <sevajol.bastien@gmail.com>
2021-03-28 18:43:37 +0200

    bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ dev_require = ["black", "isort", "pip-utils", "mypy"] + test_require
 
 setup(
     name="rolling",
-    version="0.7.2",
+    version="0.7.3",
     description="Role game engine",
     author="Bastien Sevajol",
     author_email="sevajol.bastien@gmail.com",
