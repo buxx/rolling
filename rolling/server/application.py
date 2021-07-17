8fd42d3b8e3b1305b1519fc73e7009e3b13cf090
Bastien Sevajol <sevajol.bastien@gmail.com>
2021-07-17 14:35:35 +0200

    no auth for ws

--- rolling/server/application.py
+++ rolling/server/application.py
@@ -51,6 +51,7 @@ def get_application(kernel: Kernel, disable_auth: bool = False) -> Application:
                 "/world/source",
             )
             and not request.path.startswith("/ac/")
+            and not request.path.startswith("/ws/")
             and not request.path.startswith("/zones/")
             and not request.path.startswith("/admin")
         ):
